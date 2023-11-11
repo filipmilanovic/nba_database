f46765dd224b48c21e74df4a8e6e680f202f7669
Filip Milanovic <filipmilanovic@gmail.com>
2023-11-11 09:00:16 +0100

    cleanup and documentation

--- src/classes/datamodel.py
+++ src/classes/datamodel.py
@@ -1,10 +1,10 @@
-
+""" Creates the DataModel object """
 import datetime as dt
 import hashlib
 import sqlalchemy as sql
 
 class DataModel:
-    """ Create object used as interfact to postgres data model """
+    """ Create object used to prepare data for postgres ingestion """
     def __init__(self,
                  target_table: str,
                  parameters: dict,
@@ -17,13 +17,22 @@ class DataModel:
         self.conn = kwargs['conn']
         self.response_fields = None
         self.response_values = None
-        self.key_indices = {}
+        self.unique_fields = None
         self.parsed_data = None
         self.model = None
 
     def get_response_components(self,
                                 endpoint):
-        # if the response deviates from the header/rowset standard, have to manually define the keys used in the response
+        """
+        Takes response from the NBAEndpoint object, then extracts the relevant
+        data components (i.e. response values and corresponding fields,
+        if required, using parameters.json)
+        
+        Args:
+            endpoint (_type_): the response from the relevant NBAEndpoint object
+        """
+        # if the response deviates from the header/rowset standard, have to
+        # manually define the keys used in the response
         if self.parameters.get('response_keys'):
             response = endpoint.response
             for key in self.parameters['response_keys']:
@@ -34,41 +43,63 @@ class DataModel:
             self.response_values = endpoint.response['resultSets'][0]['rowSet']
     
     def get_key_indices(self):
-        # if the response deviates from the header/rowset standard, then response is already dict
+        """
+        Uses parameters.json to get the relevant field or index required
+        to generate unique keys from the data
+        """
+        # if the response deviates from the header/rowset standard, then
+        # response is already dict, so we just need the key name
         if self.parameters.get('response_keys'):
-            pass
+            self.unique_fields = {
+                f'{key}_INDEX': key
+                for key in self.parameters['unique_keys']
+            }
+        # otherwise we have to find the corresponding index
         else:
-            self.key_indices = {f'{key}_INDEX': self.response_fields.index(key) for key in self.parameters['unique_keys']}
+            self.unique_fields = {
+                f'{key}_INDEX': self.response_fields.index(key)
+                for key in self.parameters['unique_keys']
+            }
 
     def parse_data(self):
-        if self.parameters.get('response_keys'):
-            self.parsed_data = [
-                {
-                    'id':
-                        hashlib.md5(
-                            bytes(' '.join([str(row[index]) for index in self.parameters['unique_keys']]), encoding='utf-8')
-                        ).hexdigest(),
-                    'raw_data':
-                        row[self.parameters['data_key']]
-                }
-                for row in self.response_values
-            ]
-        else:
-            self.parsed_data = [
-                {
-                    'id':
-                        hashlib.md5(
-                            bytes(' '.join([str(row[index]) for index in self.key_indices.values()]), encoding='utf-8')
-                        ).hexdigest(),
-                    'raw_data':
-                        {
-                            self.response_fields[i]: row[i] for i in range(len(self.response_fields))
-                        }
-                }
-                for row in self.response_values
-            ]
+        """
+        Generates a json-style object with a unique id for each element in
+        the raw response, as well as the corresponding element
+        """
+        self.parsed_data = [
+            {
+                # since nba API doesn't guarantee unique keys, we need to
+                # manually generate them
+                'id':
+                    hashlib.md5(
+                        bytes(
+                            ' '.join(
+                                [
+                                    str(row[field])
+                                    for field in self.unique_fields.values()
+                                 ]
+                                ),
+                            encoding='utf-8'
+                        )
+                    ).hexdigest(),
+                'raw_data':
+                    # if the response deviates from the header/rowset standard
+                    # then it's already a dict
+                    row[self.parameters['data_key']]
+                        if self.parameters.get('response_keys')
+                    # otherwise generate the dict ourselves
+                    else {
+                        self.response_fields[i]: row[i]
+                        for i in range(len(self.response_fields))
+                    }
+            }
+            for row in self.response_values
+        ]
     
     def create_raw_table(self):
+        """
+        Creates the nba.raw table if it does not already exist.
+        """
         self.model = sql.Table(self.target_table,
             self.meta,
             sql.Column('id', sql.VARCHAR, primary_key=True),
@@ -79,7 +110,13 @@ class DataModel:
         self.meta.create_all(self.eng)
     
     def load_raw_data(self):
-        insert_stmt = sql.dialects.postgresql.insert(self.meta.tables[f'raw.{self.target_table}']).values(self.parsed_data)
+        """
+        Inserts the parsed data, matchin id and raw_data to the corresponding
+        fields
+        """
+        insert_stmt = sql.dialects.postgresql.insert(
+            self.meta.tables[f'raw.{self.target_table}']
+        ).values(self.parsed_data)
 
         insert_stmt = insert_stmt.on_conflict_do_nothing(index_elements=['id'])
 
